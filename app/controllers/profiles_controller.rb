baac9c51e95f62888feb1ca8406a2de1eb816afa
First Last <yu.wesley@yahoo.com.au>
2017-01-01 01:31:41 +0000

    Added profile update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,22 +7,39 @@ class ProfilesController < ApplicationController
 	end
 	
 	# POST to /users/:user_id/profile
-	def create
-		@user = User.find(params[:user_id])
-		# Create profile linked to this specific user
-		@profile = @user.build_profile( profile_params )
-		if @profile.save
-			flash[:success] = "Profile updated!"
-			redirect_to user_path ( params[:user_id] )
-		else
-			render action :new
-		end
-	end
+  def create
+    # Ensure that we have the user who is filling out form
+    @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :new
+    end
+  end
 	
 	# GET to /users/:user_id/profile/edit
 	def edit
 		@user = User.find( params[:user_id] )
 		@profile = @user.profile
+	end
+	
+	# PUT to /users/:user_id/profile
+	def update
+		# Retrieve user from the database
+		@user = User.find( params[:user_id] )
+		# Retrieve that users profile
+		@profile = @user.profile
+		# Mass assign edited profile attributes and save (update)
+		if @profile.update_attributes(profile_params)
+			flash[:success] = "Profile Updated!"
+			redirect_to user_path(id: params[:user_id])
+			# Redirect user to their profile page
+		else
+			render action: :edit
+		end
 	end
 	
 	private
